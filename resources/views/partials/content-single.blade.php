effd7d2f301ed3a57a18e6a5d2aa75927072ee75
Chris Violette <cvioletterun@gmail.com>
2018-01-03 16:46:21 -0500

    Updating style of single posts

--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -2,6 +2,8 @@
   <header>
     <h1 class="entry-title">{{ get_the_title() }}</h1>
     @include('partials/entry-meta')
+    {{ the_post_thumbnail('large') }}
+    <hr>
   </header>
   <div class="entry-content">
     @php(the_content())
@@ -9,5 +11,4 @@
   <footer>
     {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
   </footer>
-  @php(comments_template('/partials/comments.blade.php'))
 </article>
--- resources/views/partials/entry-meta.blade.php
+++ resources/views/partials/entry-meta.blade.php
@@ -1,6 +1,14 @@
-<time class="updated" datetime="{{ get_post_time('c', true) }}">{{ get_the_date() }}</time>
-<p class="byline author vcard">
-  {{ __('By', 'sage') }} <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">
-    {{ get_the_author() }}
-  </a>
+<p class="entry-meta">
+  <span class="byline author vcard">
+    <i class="fa fa-user"></i> 
+    <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">{{ get_the_author() }}</a>
+  </span><br>
+  <time class="updated" datetime="{{ get_post_time('c', true) }}">
+    <i class="fa fa-clock-o"></i>
+    {{ get_the_date('l, F j, Y \a\t g:i a') }}</time>
 </p>
+<div class="entry-categories">
+  @foreach( get_the_category() as $category )
+    <a class="badge badge-primary" href="{{ get_category_link( $category->term_id ) }}"><i class="fa fa-folder-open-o"></i> {{ $category->name }}</a>
+  @endforeach
+</div>
