90a29091659589a659e59ae730070c8753d5dac7
Chris Violette <cvioletterun@gmail.com>
2018-01-15 13:04:51 -0500

    Updating display of frontpage announcements

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -2,31 +2,46 @@
 
 @section('announcements')
 
-  @php( query_posts( array(
-    'category_name' => 'announcements',
-    'showposts' => 10,
-  ) ) )
+  <div class="announcements">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-9 announcements-current">
+          @php( query_posts( array(
+            'category_name' => 'announcements',
+            'showposts' => 10,
+          ) ) )
 
-  @if (have_posts())
-    @php( $c = 1 )
-    <div class="announcements">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-10 announcements-current">
-            @while (have_posts()) @php(the_post())
+          @if( have_posts() )
+            @php( $c = 1 )
+            @while( have_posts() ) @php( the_post() )
               @if( $c == 4 )
                 <div class="announcements-current--secondary">
               @endif
+
               @include('partials.content-announcement-'.get_post_type())
               @php( $c++ )
             @endwhile
-            </div>
-          </div>
-          <div class="col-md-2 announcements-past"></div>
-        </div>
-      </div>
-    </div>
-  @endif
+          </div>{{-- /.announcements-current--secondary --}}
+          @endif
+
+        </div>{{-- /.announcements-current --}}
+        <div class="col-md-3 announcements-past">
+          @php( query_posts( array(
+            'category_name' => 'announcements',
+            'showposts' => 10,
+            'offset' => 10,
+          ) ) )
+          @if( have_posts() )
+            @while( have_posts() ) @php( the_post() )
+              @include('partials.content-announcement-'.get_post_type())
+            @endwhile
+          @endif
+        </div>{{-- /.announcements-past --}}
+
+
+      </div>{{-- /.row --}}
+    </div>{{-- /.container --}}
+  </div>{{-- /.announcements --}}
 @endsection
 
 @section('content')
