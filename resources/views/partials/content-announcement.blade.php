2de9948948948b683a1cb5bc9ea627b878268406
Chris Violette <cvioletterun@gmail.com>
2018-01-16 15:02:25 -0500

    Style updates

--- resources/views/partials/content-announcement.blade.php
+++ resources/views/partials/content-announcement.blade.php
@@ -2,9 +2,11 @@
   <a href="{{ get_permalink() }}" class="featured-image">
     <img src="{!! Single::getFeaturedImageURL() !!}" alt="{{ get_the_title() }}">
   </a>
-  <h2 class="entry-title"><a href="{{ get_permalink() }}">{{ get_the_title() }}</a></h2>
-  <section class="meta">
+  <section class="entry-meta">
     <time class="updated" datetime="{{ get_post_time('c', true) }}"><i class="fa fa-clock-o"></i> {{ get_the_date() }}</time>
+  </section>
+  <h3 class="entry-title"><a href="{{ get_permalink() }}">{{ get_the_title() }}</a></h3>
+  <section class="entry-meta">
     <div class="categories">
       @foreach( get_the_category() as $category )
         <a class="badge badge-light" href="{{ get_category_link( $category->term_id ) }}"><i class="fa fa-folder-open-o"></i> {{ $category->name }}</a>
