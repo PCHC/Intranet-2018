e4a6e5ea9be5d8b5609d5cae93036c43d0b6e300
Chris Violette <cvioletterun@gmail.com>
2018-03-28 14:51:00 -0400

    Updating some global styles

--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -1,11 +1,11 @@
 <article @php(post_class())>
   <header>
-    <h1 class="entry-title">{{ get_the_title() }}</h1>
-    <div class="row entry-meta">
+    <h1 class="post--title">{{ get_the_title() }}</h1>
+    <div class="row post--meta">
       <div class="col">
-        <time class="updated" datetime="{{ get_post_time('c', true) }}">
+        <time class="post--published" datetime="{{ get_post_time('c', true) }}">
           Posted {{ get_the_date('F j, Y \a\t g:i a') }}</time>
-        <span class="byline author vcard">
+        <span class="byline author vcard post--author">
           by <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">{{ get_the_author() }}</a>
         </span>
       </div>
@@ -13,9 +13,13 @@
         @include('partials.entry-categories')
       </div>
     </div>
-    {{ the_post_thumbnail('large') }}
+    @if( has_post_thumbnail() )
+      <div class="post--image">
+        {{ the_post_thumbnail('large') }}
+      </div>
+    @endif
   </header>
-  <div class="entry-content">
+  <div class="post--content">
     @php(the_content())
   </div>
   <footer>
--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -1,25 +1,27 @@
-<article @php(post_class('row'))>
-  @if( has_post_thumbnail() )
-    <div class="col-4 col-sm-3 col-lg-2 entry-image">
-      <a href="{{ get_permalink() }}" class="featured-image">
-        {{ the_post_thumbnail('thumbnail') }}
-      </a>
-    </div>
-  @endif
+<article @php(post_class())>
+  <div class="row">
+    @if( has_post_thumbnail() )
+      <div class="col-4 col-sm-3 col-lg-2 post--image">
+        <a href="{{ get_permalink() }}" class="featured-image">
+          {{ the_post_thumbnail('thumbnail') }}
+        </a>
+      </div>
+    @endif
 
-  <div class="col">
-    <h2 class="entry-title">
-      <a href="{{ get_permalink() }}">{{ get_the_title() }}</a>
-    </h2>
-    <div class="entry-published">
-      <time class="updated" datetime="{{ get_post_time('c', true) }}">
-        {{ get_the_date('F j, Y') }}
-      </time>
+    <div class="col post--content">
+      <h2 class="post--title">
+        <a href="{{ get_permalink() }}">{{ get_the_title() }}</a>
+      </h2>
+      <div class="post--meta">
+        <time class="post--published" datetime="{{ get_post_time('c', true) }}">
+          {{ get_the_date('F j, Y') }}
+        </time>
+      </div>
+      @include('partials.entry-categories')
+      <div class="post--summary">
+        @php(the_excerpt())
+        <a class="read-more-button btn btn-sm btn-primary post--readmore" href="{{ get_permalink() }}"><i class="fa fa-angle-right"></i> Read More</a>
+      </div>
     </div>
-    @include('partials.entry-categories')
-    <div class="entry-summary">
-      @php(the_excerpt())
-      <a class="read-more-button btn btn-sm btn-primary" href="{{ get_permalink() }}"><i class="fa fa-angle-right"></i> Read More</a>
-    </div>
-  </section>
+  </div>
 </article>
