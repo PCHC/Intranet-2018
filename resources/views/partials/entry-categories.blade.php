5bb47ee1643e875af7311b8df91d946c05b6c11f
Chris Violette <cvioletterun@gmail.com>
2018-04-20 11:16:40 -0400

    Updating some front page styles.

--- resources/views/partials/entry-categories.blade.php
+++ resources/views/partials/entry-categories.blade.php
@@ -1,5 +1,5 @@
 <div class="post--categories">
   @foreach( get_the_category() as $category )
-    <a href="{{ get_category_link( $category->term_id ) }}"><i class="fa fa-folder-open-o"></i> {{ $category->name }}</a>
+    <a href="{{ get_category_link( $category->term_id ) }}"><i class="fas fa-folder-open"></i> {{ $category->name }}</a>
   @endforeach
 </div>
