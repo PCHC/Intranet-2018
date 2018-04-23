f2595fd1b623661776c52b7450641d26300938f8
Chris Violette <cvioletterun@gmail.com>
2018-04-23 10:12:30 -0400

    Adding dropdown icon to nav elements with children

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,6 +1,11 @@
 export default {
   init() {
     // JavaScript to be fired on all pages
+
+    // Add indicator that menu item has children
+    $('.menu-item-has-children > a').each(function() {
+      $(this).append(' <i class="fa fa-angle-down"></i>');
+    });
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
