b67d29b46ea3df25de393385d3b2521dbb53830d
Chris Violette <cvioletterun@gmail.com>
2018-04-24 12:21:07 -0400

    Fix

---
+++ resources/assets/scripts/autoload/_fontawesome.js
@@ -0,0 +1,5 @@
+// import fontawesome from '@fortawesome/fontawesome';
+// import solid from '@fortawesome/fontawesome-free-solid';
+// import regular from '@fortawesome/fontawesome-free-regular';
+// 
+// fontawesome.library.add(solid,regular);
