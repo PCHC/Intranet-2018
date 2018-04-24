9fb839d6090d477d62a012e90c356868e5fa6f5e
Chris Violette <cvioletterun@gmail.com>
2018-04-24 11:47:51 -0400

    Switching up how we handle FontAwesome

--- resources/assets/scripts/autoload/_fontawesome.js
+++ resources/assets/scripts/autoload/_fontawesome.js
@@ -1,4 +1,5 @@
-//import fontawesome from '@fortawesome/fontawesome';
-//import solid from '@fortawesome/fontawesome-free-solid';
+import fontawesome from '@fortawesome/fontawesome';
+import solid from '@fortawesome/fontawesome-free-solid';
+import regular from '@fortawesome/fontawesome-free-regular';
 
-//fontawesome.library.add(solid);
+fontawesome.library.add(solid,regular);
