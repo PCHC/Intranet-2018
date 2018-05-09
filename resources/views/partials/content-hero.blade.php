ddac2c5b4df8d661e4fc44c6de9aeff682d74e98
Chris Violette <cvioletterun@gmail.com>
2018-05-09 15:43:46 -0400

    Updating styling of posts

--- resources/views/partials/content-hero.blade.php
+++ resources/views/partials/content-hero.blade.php
@@ -1,24 +1,28 @@
 <article @php(post_class())>
-  @if( has_post_thumbnail() )
-    <a href="{{ get_permalink() }}" class="featured-image">
-      {{ the_post_thumbnail('og-image') }}
-    </a>
-  @endif
+  <div class="post--wrap">
+    @if( has_post_thumbnail() )
+      <div class="post--image">
+        <a href="{{ get_permalink() }}" class="featured-image">
+          {{ the_post_thumbnail('og-image') }}
+        </a>
+      </div>
+    @endif
 
-  @include('partials.entry-categories')
-
-  <h2 class="post--title">
-    <a href="{{ get_permalink() }}">
-      {{ the_title() }}
-    </a>
-  </h2>
-  <div class="post--meta">
-    <time class="post--published" datetime="{{ get_post_time('c', true) }}">
-      {{ get_the_date() }}
-    </time>
+    <div class="post--content">
+      <h2 class="post--title">
+        <a href="{{ get_permalink() }}">{{ get_the_title() }}</a>
+      </h2>
+      <div class="post--meta">
+        <time class="post--published" datetime="{{ get_post_time('c', true) }}">
+          {{ get_the_date('F j, Y') }}
+        </time>
+      </div>
+      @include('partials.entry-categories')
+      @include('partials.recognition-taxonomies')
+      <div class="post--summary">
+        @php(the_excerpt())
+        <a class="read-more-button btn btn-sm btn-primary post--readmore" href="{{ get_permalink() }}"><i class="fa fa-angle-right"></i> Read More</a>
+      </div>
+    </div>
   </div>
-  <section class="post--summary">
-    @php(the_excerpt())
-    <a class="read-more-button btn btn-sm btn-primary post--readmore" href="{{ get_permalink() }}"><i class="fa fa-angle-right"></i> Read More</a>
-  </section>
 </article>
