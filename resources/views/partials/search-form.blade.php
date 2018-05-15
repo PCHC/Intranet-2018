b0c8eb78979892bcbbb5ac00bef9fc9156584b0d
Chris Violette <cvioletterun@gmail.com>
2018-05-15 15:01:14 -0400

    More search form styling.

--- resources/views/partials/search-form.blade.php
+++ resources/views/partials/search-form.blade.php
@@ -1,11 +1,10 @@
 <div class="search">
   <form role="search" method="get" class="search--form" action="{{ home_url('/') }}">
-  	<label class="search--label">
-  		<span class="screen-reader-text">Search for:</span>
   		<input class="search--field" placeholder="Search &hellip;" aria-label="Search" value="{{ sanitize_text_field( get_search_query() ) }}" name="s" type="search">
-  	</label>
-  	<button class="search--submit" value="Search" type="submit" aria-label="Submit">
-      <i class="fa fa-search"></i>
-    </button>
+      <div class="search--input-group">
+      	<button class="search--submit" value="Search" type="submit" aria-label="Submit">
+          <i class="fa fa-search"></i>
+        </button>
+      </div>
   </form>
 </div>
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -4,6 +4,25 @@
   @include('partials.page-header')
   @include('partials.search-form')
 
+  <div class="card border-info mt-3 mb-3">
+    <div class="card-header">
+      <strong>Looking for Forms &amp; Documents?</strong>
+    </div>
+    <div class="card-body">
+      <p>Try the following search:</p>
+      <div class="search">
+        <form role="search" method="get" class="search--form" action="{{ home_url('/resources/forms-documents/') }}">
+      		<input class="search--field" placeholder="Search Forms &amp; Documents &hellip;" aria-label="Search Forms &amp; Documents" value="{{ sanitize_text_field( get_search_query() ) }}" name="wpfb_s" type="search">
+          <div class="search--input-group">
+          	<button class="search--submit" value="Search" type="submit" aria-label="Submit">
+              <i class="fa fa-search"></i>
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
   <hr />
 
   @if (!have_posts())
@@ -16,6 +35,7 @@
     <div class="posts--wrap">
       @while(have_posts()) @php(the_post())
         @include('partials.content')
+        <hr />
       @endwhile
     </div>
   @endif
