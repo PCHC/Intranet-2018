47202f72d1286d5a491e5b9f8d6aa9ffc613fe4d
Chris Violette <cvioletterun@gmail.com>
2018-05-16 16:15:28 -0400

    Making fluid containers up to a certain width.

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="content-info document-footer">
-  <div class="container">
+  <div class="container-fluid container--footer">
     <div class="footer--row">
       @php(dynamic_sidebar('sidebar-footer'))
     </div>
