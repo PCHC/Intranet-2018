429d2e46ef9c02cdaf655a5812d6a23bafed1157
Chris Violette <cvioletterun@gmail.com>
2018-05-22 14:42:16 -0400

    Made the child pages sidebar more smarterer

--- resources/views/partials/child-pages.blade.php
+++ resources/views/partials/child-pages.blade.php
@@ -9,11 +9,28 @@
       'echo' => false,
     );
     $pageChildren = wp_list_pages($pageChildrenArgs);
+    $postID = $post->ID;
+
+    if( !$pageChildren ) {
+      $postID = wp_get_post_parent_id( $post->ID );
+
+      if( $postID !== 0 ) {
+        $pageChildrenArgs = array(
+          'child_of' => $postID,
+          'depth' => 1,
+          'title_li' => null,
+          'echo' => false,
+        );
+        $pageChildren = wp_list_pages($pageChildrenArgs);
+      }
+    }
   ?>
   @if( $pageChildren )
     <aside class="page-children">
-      <ul class="page-children--nav">
-        {!! wp_list_pages("title_li=&include=".$post->ID."&echo=0&depth=3") !!}
+      <ul class="page-children--nav page-children--nav__parent">
+        {!! wp_list_pages("title_li=&include=".$postID."&echo=0&depth=3") !!}
+      </ul>
+      <ul class="page-children--nav page-children--nav__children">
         {!! $pageChildren !!}
       </ul>
     </aside>
