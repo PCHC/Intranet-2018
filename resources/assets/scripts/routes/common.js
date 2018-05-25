05c88dcbb3086a5cc065eefd5fbdb5096c3bfe94
Chris Violette <cvioletterun@gmail.com>
2018-05-25 14:25:23 -0400

    Trying a new internal link styling

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -10,6 +10,10 @@ export default {
     $('.menu-item-has-children > a').each(function() {
       $(this).append(' <i class="fa fa-angle-down"></i>');
     });
+
+    $('main a:not(.btn)').each(function() {
+      $(this).wrapInner('<span></span>').addClass('link-effect');
+    });
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
