be2bb6fb863d236c7f3c5b1edb8d18ae947a3bbd
Chris Violette <cvioletterun@gmail.com>
2018-06-05 15:00:13 -0400

    Testing SMTP config.

--- resources/functions.php
+++ resources/functions.php
@@ -97,3 +97,15 @@ Container::getInstance()
 foreach( glob( dirname(__DIR__) . "/app/plugins/**/*.php") as $file ) {
   require $file;
 }
+
+add_action( 'phpmailer_init', function( $phpmailer ) {
+  $phpmailer->isSMTP();
+  $phpmailer->Host       = SMTP_HOST;
+  $phpmailer->SMTPAuth   = SMTP_AUTH;
+  $phpmailer->Port       = SMTP_PORT;
+  $phpmailer->Username   = SMTP_USER;
+  $phpmailer->Password   = SMTP_PASS;
+  $phpmailer->SMTPSecure = SMTP_SECURE;
+  $phpmailer->From       = SMTP_FROM;
+  $phpmailer->FromName   = SMTP_NAME;
+});
