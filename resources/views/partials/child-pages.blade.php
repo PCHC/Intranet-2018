1d50e64c1a48da851390129a7770586cd1d8c2a3
Chris Violette <cvioletterun@gmail.com>
2018-06-14 14:17:22 -0400

    Updating page children navigation

--- resources/views/partials/child-pages.blade.php
+++ resources/views/partials/child-pages.blade.php
@@ -2,37 +2,101 @@
   <?php
     the_post();
 
-    $pageChildrenArgs = array(
+    // Get current page
+    $pgSelfArgs = array(
+      'include' => $post->ID,
+      'depth' => 1,
+      'title_li' => null,
+      'echo' => false,
+    );
+    $pgSelfOutput = wp_list_pages($pgSelfArgs);
+
+    // Get siblings of current page
+    $pgSiblings = get_pages('child_of='.$post->ID);
+    $pgSiblingsArgs = array(
+      'child_of' => $post->post_parent,
+      'depth' => 1,
+      'title_li' => null,
+      'echo' => false,
+    );
+    $pgSiblingsOutput = wp_list_pages($pgSiblingsArgs);
+
+    // Get children of current page
+    $pgChildrenArgs = array(
       'child_of' => $post->ID,
       'depth' => 1,
       'title_li' => null,
       'echo' => false,
     );
-    $pageChildren = wp_list_pages($pageChildrenArgs);
-    $postID = $post->ID;
+    $pgChildrenOutput = wp_list_pages($pgChildrenArgs);
 
-    if( !$pageChildren ) {
-      $postID = wp_get_post_parent_id( $post->ID );
+    // Get parent page if there is one
+    if($post->post_parent) {
+      $pgParent = get_pages('include='.$post->post_parent);
+      $pgParent = $pgParent[0];
 
-      if( $postID !== 0 ) {
-        $pageChildrenArgs = array(
-          'child_of' => $postID,
+      $pgParentArgs = array(
+        'include' => $post->post_parent,
+        'depth' => 1,
+        'title_li' => null,
+        'echo' => false,
+        'link_before' => '<span class="page-children--nav__icon"><i class="fas fa-angle-left"></i></span><span>',
+        'link-after' => '</span>'
+      );
+      $pgParentOutput = wp_list_pages($pgParentArgs);
+
+      // Get grandparent page if there is one
+      if($pgParent->post_parent) {
+        $pgGrandparent = get_pages('include='.$pgParent->post_parent);
+        $pgGrandparent = $pgGrandparent[0];
+        $pgGrandparentArgs = array(
+          'include' => $pgGrandparent->ID,
           'depth' => 1,
           'title_li' => null,
           'echo' => false,
+          'link_before' => '<span class="page-children--nav__icon"><i class="fas fa-angle-double-left"></i></span><span>',
+          'link-after' => '</span>'
         );
-        $pageChildren = wp_list_pages($pageChildrenArgs);
+        $pgGrandparentOutput = wp_list_pages($pgGrandparentArgs);
       }
     }
   ?>
-  @if( $pageChildren )
+  @if( $pgParentOutput || $pgChildrenOutput )
     <aside class="page-children">
-      <ul class="page-children--nav page-children--nav__parent">
-        {!! wp_list_pages("title_li=&include=".$postID."&echo=0&depth=3") !!}
-      </ul>
-      <ul class="page-children--nav page-children--nav__children">
-        {!! $pageChildren !!}
-      </ul>
+      @if( $pgGrandparentOutput && !$pgChildrenOutput )
+        <strong>Grandparent</strong>
+        <ul class="page-children--nav page-children--nav__grandparent">
+          {!! $pgGrandparentOutput !!}
+        </ul>
+      @endif
+
+      @if( $pgParentOutput )
+        <strong>Parent</strong>
+        <ul class="page-children--nav page-children--nav__parent">
+          {!! $pgParentOutput !!}
+        </ul>
+      @endif
+
+      @if( ($pgSelfOutput && $pgChildrenOutput ) )
+        <strong>Self</strong>
+        <ul class="page-children--nav page-children--nav__self">
+          {!! $pgSelfOutput !!}
+        </ul>
+      @endif
+
+      @if( $pgSiblingsOutput && !$pgChildrenOutput )
+        <strong>Siblings</strong>
+        <ul class="page-children--nav page-children--nav__siblings">
+          {!! $pgSiblingsOutput !!}
+        </ul>
+      @endif
+
+      @if( $pgChildrenOutput )
+        <strong>Children</strong>
+        <ul class="page-children--nav page-children--nav__children">
+          {!! $pgChildrenOutput !!}
+        </ul>
+      @endif
     </aside>
   @endif
 @endif
