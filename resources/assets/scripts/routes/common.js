2942aabf558be7309bb20d63a54609c138b1a32e
Chris Violette <cvioletterun@gmail.com>
2018-06-19 15:33:53 -0400

    Adding icon to external links

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -11,7 +11,7 @@ export default {
     $('a').each(function() {
       var a = new RegExp('/' + window.location.host + '/');
       if(!a.test(this.href)) {
-        $(this).attr("target","_blank");
+        $(this).attr("target","_blank").append(' <small><i class="fas fa-external-link-alt"></i></small>');
       }
     });
 
