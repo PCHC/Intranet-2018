d556d5454eb1ef75247e8f34ac7af9b746de8e25
Chris Violette <cvioletterun@gmail.com>
2018-07-03 12:18:59 -0400

    Updating email

--- app/actions.php
+++ app/actions.php
@@ -29,7 +29,7 @@ add_action( 'send_email_digest', function () {
 
   if( $digestQuery->have_posts() ) {
 
-    $body .= '<table width="600" cellspacing="0" cellpadding="0" border="0" style="boder-collapse: collapse; width: 600px;">';
+    $body .= '<table width="700" cellspacing="0" cellpadding="8" border="0" style="boder-collapse: collapse; width: 700px;">';
 
     while( $digestQuery->have_posts() ) {
       $digestQuery->the_post();
@@ -37,9 +37,9 @@ add_action( 'send_email_digest', function () {
       $body .= '<tr>';
 
         if( has_post_thumbnail() ) {
-          $body .= '<td>';
+          $body .= '<td style="vertical-align: top;">';
             $body .= '<a href="'.get_the_permalink().'?utm_source=latest-news&utm_medium=email&utm_content=thumbnail">';
-              $body .= '<img src="'.get_site_url().'/'.get_the_post_thumbnail_url(get_the_ID(), 'thumbnail').'" style="margin-right: 12px;" alt="'.get_the_title().'"/>';
+              $body .= '<img src="'.get_site_url().'/'.get_the_post_thumbnail_url(get_the_ID(), 'thumbnail').'" style="margin-right: 12px; width: 100px;" alt="'.get_the_title().'"/>';
             $body .= '</a>';
           $body .= '</td>';
         }
@@ -47,14 +47,14 @@ add_action( 'send_email_digest', function () {
         $body .= has_post_thumbnail() ? '<td>' : '<td colspan="2">';
 
           $body .= '<h2 style="font-size: 24px; font-weight: bold; margin-top:0;"><a href="'.get_the_permalink().'?utm_source=latest-news&utm_medium=email&utm_content=title">'.get_the_title().'</a></h2>';
-          $body .= "<p style=\"margin-bottom: 18px;\"><em>" . get_the_date('F j, Y') . " &mdash; </em>" . get_the_excerpt() . "</p>";
+          $body .= "<p style=\"margin-bottom: 36px;\"><em>" . get_the_date('F j, Y') . " &mdash; </em>" . get_the_excerpt() . "</p>";
         $body .= '</td>';
       $body .= '</tr>';
     }
 
     $body .= '</table>';
 
-    $body .= '<p><strong>Stay up to date by visiting the <a href="' . get_home_url() . '?utm_source=latest-news&utm_medium=email&utm_content=bottomlink">PCHC Employee Intranet</a>.</strong></p>';
+    $body .= '<hr/><p><strong>Stay up to date by visiting the <a href="' . get_home_url() . '?utm_source=latest-news&utm_medium=email&utm_content=bottomlink">PCHC Employee Intranet</a>.</strong></p>';
     wp_mail($to, $subject, $body, $headers);
   }
 
