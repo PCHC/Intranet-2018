7766b92f3c61370bcbc1de85e88afaf871f4eb7b
Chris Violette <cvioletterun@gmail.com>
2018-07-03 12:09:21 -0400

    Updating email to table-based layout

--- app/actions.php
+++ app/actions.php
@@ -3,48 +3,60 @@
 namespace App;
 
 /**
- * Custom actions
- */
- add_action( 'send_email_digest', function () {
-   $args = array(
-     'post_type' => 'post',
-     'post_status' => 'publish',
-     'orderby' => 'date',
-     'order' => 'DESC',
-     'date_query' => array(
-       array(
-         'after' => '1 week ago'
-       ),
-     ),
-   );
+* Custom actions
+*/
+add_action( 'send_email_digest', function () {
+  $args = array(
+    'post_type' => 'post',
+    'post_status' => 'publish',
+    'orderby' => 'date',
+    'order' => 'DESC',
+    'date_query' => array(
+      array(
+        'after' => '1 week ago'
+      ),
+    ),
+  );
 
-   $to = 'cviolette@pchc.com';
-   $subject = 'Latest PCHC Employee News - ' . date('F j, Y');
-   $body = "<p>Here's what you may have missed this week:</p>";
-   $headers = array(
-     'Content-Type: text/html; charset=UTF-8'
-   );
+  $to = 'cviolette@pchc.com';
+  $subject = 'Latest PCHC Employee News - ' . date('F j, Y');
+  $body = "<p>Here's what you may have missed this week:</p>";
+  $headers = array(
+    'Content-Type: text/html; charset=UTF-8'
+  );
 
-   $digestQuery = new \WP_Query( $args );
+  $digestQuery = new \WP_Query( $args );
 
-   if( $digestQuery->have_posts() ) {
-     while( $digestQuery->have_posts() ) {
-       $digestQuery->the_post();
+  if( $digestQuery->have_posts() ) {
 
-       if( has_post_thumbnail() ) {
-         $body .= '<a href="'.get_the_permalink().'?utm_source=latest-news&utm_medium=email&utm_content=thumbnail">';
-          $body .= '<img src="'.get_site_url().'/'.get_the_post_thumbnail_url(get_the_ID(), 'thumbnail').'" style="float: left; margin-right: 12px; max-width: 100px;" alt="'.get_the_title().'"/>';
-         $body .= '</a>';
-       }
+    $body .= '<table width="600" cellspacing="0" cellpadding="0" border="0" style="boder-collapse: collapse; width: 600px;">';
 
-       $body .= '<h2 style="font-size: 24px; font-weight: bold; margin-top:0;"><a href="'.get_the_permalink().'?utm_source=latest-news&utm_medium=email&utm_content=title">'.get_the_title().'</a></h2>';
-       $body .= "<p><em>" . get_the_date('F j, Y') . " &mdash; </em>" . get_the_excerpt() . "</p>";
-       $body .= '<hr style="clear: both;"/>';
-     }
+    while( $digestQuery->have_posts() ) {
+      $digestQuery->the_post();
 
-     $body .= '<br/><p><strong>Stay up to date by visiting the <a href="' . get_home_url() . '?utm_source=latest-news&utm_medium=email&utm_content=bottomlink">PCHC Employee Intranet</a>.</strong></p>';
-     wp_mail($to, $subject, $body, $headers);
-   }
+      $body .= '<tr>';
 
-   wp_reset_postdata();
- } );
+        if( has_post_thumbnail() ) {
+          $body .= '<td>';
+            $body .= '<a href="'.get_the_permalink().'?utm_source=latest-news&utm_medium=email&utm_content=thumbnail">';
+              $body .= '<img src="'.get_site_url().'/'.get_the_post_thumbnail_url(get_the_ID(), 'thumbnail').'" style="margin-right: 12px;" alt="'.get_the_title().'"/>';
+            $body .= '</a>';
+          $body .= '</td>';
+        }
+
+        $body .= has_post_thumbnail() ? '<td>' : '<td colspan="2">';
+
+          $body .= '<h2 style="font-size: 24px; font-weight: bold; margin-top:0;"><a href="'.get_the_permalink().'?utm_source=latest-news&utm_medium=email&utm_content=title">'.get_the_title().'</a></h2>';
+          $body .= "<p style=\"margin-bottom: 18px;\"><em>" . get_the_date('F j, Y') . " &mdash; </em>" . get_the_excerpt() . "</p>";
+        $body .= '</td>';
+      $body .= '</tr>';
+    }
+
+    $body .= '</table>';
+
+    $body .= '<p><strong>Stay up to date by visiting the <a href="' . get_home_url() . '?utm_source=latest-news&utm_medium=email&utm_content=bottomlink">PCHC Employee Intranet</a>.</strong></p>';
+    wp_mail($to, $subject, $body, $headers);
+  }
+
+  wp_reset_postdata();
+} );
