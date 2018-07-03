2cfaabb4794f5599eb2eaf0ff651e89646e515fc
Chris Violette <cvioletterun@gmail.com>
2018-07-03 12:49:44 -0400

    Fixed first edition email language; added future language.

--- app/actions.php
+++ app/actions.php
@@ -20,9 +20,11 @@ add_action( 'send_email_digest', function () {
 
   $to = 'cviolette@pchc.com';
   $subject = 'Latest PCHC Employee News - ' . date('F j, Y');
-  $body = "<p>To keep you well-informed, PCHC's Employee Intranet has a new feature to email you a review of the news articles posted in the past week.</p>";
+  $body = "<p>To keep you well-informed, PCHC's Employee Intranet has a new feature to email you a review of the news articles posted recently.</p>";
+  // $ body = "<p>To keep you well-informed, PCHC's Employee Intranet emails a weekly review of the news articles posted recently.";
   $body .= "<p>This first edition will feature articles posted in the last 2 weeks. Future editions will come out on Fridays and feature articles posted during the past week.</p>";
   $body .= "<p><em>Here's what you may have missed:</em></p>";
+  // $body .= "<p><em>Here's what you may have missed this week:</em></p>";
   $headers = array(
     'Content-Type: text/html; charset=UTF-8'
   );
